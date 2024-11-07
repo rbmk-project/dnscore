7ac7f42fda909f168b74498c23b6d7abe5bd96f7
Simone Basso <bassosimone@gmail.com>
2024-11-07 12:09:56 +0100

    feat(dnscoretest): add missing test

---
+++ dnscoretest/server_test.go
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package dnscoretest
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/rbmk-project/dnscore/internal/mocks"
+)
+
+func TestServer_Close(t *testing.T) {
+	expected := errors.New("mocked error")
+	srv := &Server{}
+
+	srv.ioclosers = append(srv.ioclosers, &mocks.Conn{
+		MockClose: func() error {
+			return nil
+		},
+	})
+
+	srv.ioclosers = append(srv.ioclosers, &mocks.Conn{
+		MockClose: func() error {
+			return expected
+		},
+	})
+
+	srv.ioclosers = append(srv.ioclosers, &mocks.Conn{
+		MockClose: func() error {
+			return nil
+		},
+	})
+
+	if err := srv.Close(); !errors.Is(err, expected) {
+		t.Fatal("expected", expected, ", got", err)
+	}
+}
